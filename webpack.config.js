e3fc2e42d9c0c55ed41a1e967176a09aff132b62
Kmaschta <kevin@marmelab.com>
2019-01-23 16:13:25 +0100

    Build it

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const dist = path.resolve(__dirname, 'dist');
+const dist = path.resolve(__dirname, 'docs');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 
 module.exports = {
