0c8595ccff478924f0ab8e2fac8b09365d5d70db
Alexis Janvier <contact@alexisjanvier.net>
2019-01-23 13:35:43 +0100

    Javascript implementation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
+const primes_up_to = max => {
+    var sieve = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+            for (j = i << 1; j <= max; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    return primes;
+};
+
 import('../crate/pkg').then(module => {
-    console.log(module.add_one(41));
-    console.log('test', module.primes_up_to(300));
+    console.log('test Rust', module.primes_up_to(300));
+    console.log('test Javascript', primes_up_to(300));
 });
